1f29b1a69c9aa1ed013008b071f760c97dbc4216
pkryger <pkryger@35dc1bf1-6436-0410-8866-b360a3aefa2f>
2007-08-26 15:16:53 +0000

    Refactoring of Logger class to make in more like an object

--- branches/i004/logging.js
+++ branches/i004/logging.js
@@ -4,11 +4,11 @@ into the specified destination element innerHTML. The user shall use factory met
 in order to instantiate a logger.
 
 *** User API:
-+ static methods:
+** static methods:
 
 - getLogger(loggerName) - returns a Logger instance with a specified loggerName
 
-+ static fields:
+** static fields:
 
 - loggingLevel - the current logging level. It could be dynamically changed while application is running. The
 level change affects all further logging calls. It is calculated as a bit mask.
@@ -23,7 +23,7 @@ to the output's innerHTML
 - LEVEL_TRACE - log traces
 - LEVEL_ALL - log everything
 
-+ methods
+** methods
 
 - getName() - returns the logger instance name
 
@@ -35,186 +35,460 @@ to the output's innerHTML
 
 - trace(messagem, parameters) - logs specified message with an optional array parameters as a trace
 
-+ functions
+** functions
 
 _testLogger(out) - sandbox for Logger class.
 */
 
 
-// The constructor
-function Logger(loggerName) {
-    this._setName(loggerName);
-}
+/**
+ * The Logger class.
+ */
+var Logger = (function() {
+    /**
+     * Creates the instance of Logger with given loggerName.
+     * Note: this constructor is private. One shall use Logger.getLogger(loggetName) to obtain a logger name
+     *
+     * Parameters:
+     *   loggerName  the object to identify the logger name.
+     */
+    function constructor(loggerName)
+    {
+	if (constructor.caller != constructor.getLogger) {
+	    throw new Error("Only Logger.getLogger can create a Logger instance!" + constructor.caller);
+	}
+	/**
+	 * By convention, we make a private self parameter. This is used to make the object
+	 * available to the private methods.
+	 * This is a workaround for an error in the ECMAScript Language Specification which
+	 * causes this to be set incorrectly for inner functions.
+	 */
+	var self = this;
 
-// Factory method
-Logger._loggers = new Array();
-Logger.getLogger = function(loggerName) {
-    if (null == Logger._loggers[loggerName]) {
-	Logger._loggers[loggerName] = new Logger(loggerName);
+	/**
+	 * The object that represents the name of the Logger instance
+	 */
+	var _name = loggerName;
+
+	/** 
+	 * The Logger instance name getter. The name is returned as a String.
+	 *
+	 * Return:
+	 *   The instance name as a String 
+	 */
+	this.getName = function() {
+	    return "[" + self._name.toString() + "]";
+	}
+	
+	/**
+	 * Appends the message to the output.innerHTML if lefel is sufficient.
+	 *
+	 * Parameters:
+	 *   message - a messate to be logged
+	 *   level - the level of the message; it will be checked against loggingLevel
+	 */
+	function _print(message, level) {
+	    // Print only if loggingLevel is different than LEVEL_NONE
+	    // and level matches loggingLevel
+	    if (Logger.output != null
+		&& Logger.loggingLevel != Logger.LEVEL_NONE
+		&& (Logger.loggingLevel & level) == level) {
+		var outputHTML = Logger.output.innerHTML;
+		outputHTML += _incrementMessageNumber() + ": " + message.toString();
+		Logger.output.innerHTML = outputHTML;
+	    }
+	}
+
+	/**
+	 * Logs the error message with specified parameters to the output
+	 *
+	 * Parameters:
+	 *   message - the message to be logged
+	 *   parameters - the Array of parameters to be logged
+	 */
+	this.error = function(message, parameters) {
+	    if ((Logger.loggingLevel & Logger.LEVEL_ERROR) == Logger.LEVEL_ERROR) {
+		var params = ": " + parseArray(parameters);
+		var msg = getErrorPrefix() + message + params + getErrorSuffix() + self.getName() + " " + getEndl();
+		self._print(msg, Logger.LEVEL_ERROR);
+	    }
+	}
+
+	/**
+	 * Logs the warning message with specified parameters to the output
+	 *
+	 * Parameters:
+	 *   message - the message to be logged
+	 *   parameters - the Array of parameters to be logged
+	 */
+	this.warning = function(message, parameters) {
+	    if ((Logger.loggingLevel & Logger.LEVEL_WARNING) == Logger.LEVEL_WARNING) {
+		var params = ": " + parseArray(parameters);
+		var msg = getWarningPrefix() + message + params + getWarningSuffix() + self.getName() + " " + getEndl();
+		self._print(msg, Logger.LEVEL_WARNING);
+	    }
+	}
+
+	/**
+	 * Logs the notify message with specified parameters to the output
+	 *
+	 * Parameters:
+	 *   message - the message to be logged
+	 *   parameters - the Array of parameters to be logged
+	 */
+	this.notify = function(message, parameters) {
+	    if ((Logger.loggingLevel & Logger.LEVEL_NOTIFY) == Logger.LEVEL_NOTIFY) {
+		var params = ": " + parseArray(parameters);
+		var msg = getNotifyPrefix() + message + params + getNotifySuffix() + self.getName() + " " + getEndl();
+		self._print(msg, Logger.LEVEL_NOTIFY);
+	    }
+	}
+
+	/**
+	 * Logs the trace message with specified parameters to the output
+	 *
+	 * Parameters:
+	 *   message - the message to be logged
+	 *   parameters - the Array of parameters to be logged
+	 */
+	this.trace = function(message, parameters) {
+	    if ((Logger.loggingLevel & Logger.LEVEL_TRACE) == Logger.LEVEL_TRACE) {
+		var params = ": " + parseArray(parameters);
+		var msg = getTracePrefix() + message + params + getTraceSuffix() + self.getName() + " " + getEndl();
+		self._print(msg, Logger.LEVEL_TRACE);
+	    }
+	}
+
+    } // End of constuctor
+
+    /**
+     * The array of loggers for factory method
+     */
+    var _loggers = new Array();
+
+    /**
+     * Gives a Logger instance to be used by the user. This factory method shall be used to
+     * obtain a instance of a logger.
+     *
+     * Parameters:
+     *   loggerName the object that represents the Logger name
+     * Return:
+     *   The Logger instance for a given loggerName
+     */
+    constructor.getLogger(loggerName) {
+	if (null == _loggers[loggerName]) {
+	    _loggers[loggerName] = constructor(loggerName);
+	}
+	return _loggers[loggerName];
     }
-    return Logger._loggers[loggerName];
-}
 
-// Logging levels
+    /**
+     * One message number for all Loggers. It will be added for all messages
+     */
+    var _messageNumber = 0;
+    
+    /**
+     * Gives the message number and increments it.
+     * 
+     * Return:
+     *   The message number to be used
+     */
+    function _incrementMessageNumber() {
+	return _messageNumber++;
+    }
+    
+    /**
+     * Parses the given array and returns its string represenatation
+     * 
+     * Parameters:
+     *   array - the array object to be parsed
+     * Return:
+     *  The String in the following form: {key1:value1, key2:value2}
+     */
+    function _parseArray(array) {
+	var retString = "";
+	if (null != array) {
+	    retString = "{";
+	    for (key in array) {
+		retString += key.toString() + ":" + array[key].toString() + ", ";
+	    }
+	    retString = retString.substring(0, retString.lastIndexOf(","));
+	    retString += "}";
+	}
+	return retString;
+    }
+    
+    /**
+     * The sequence to be used as an end line
+     */
+    var _endl = "<br />";
+
+    /**
+     * The end line sequence getter
+     *
+     * Return:
+     *   The String representation of end line
+     */
+    constructor.getEndl = function() {
+	return _endl.toString();
+    }
+
+    /**
+     * The end line sequence setter
+     *
+     * Parameters:
+     *   endl - the sequence to be used as end line
+     */
+    constructor.setEndl = function(endl) {
+	_endl = endl;
+    }
+    
+    /**
+     * The sequence to be used as an error prefix
+     */
+    var _errorPrefix = "<font color=\"red\"><b>Error: ";
+    
+    /**
+     * The error prefix sequence getter
+     * 
+     * Return:
+     *    The String represenataion of error prefix
+     */
+    constructor.getErrorPrefix = function() {
+	return _errorPrefix.toString();
+    }
+
+    /**
+     * The error prefix setter
+     *
+     * Parameters:
+     *    prefix - the sequence to be used as error prefix
+     */
+    constructor.setErrorPrefix = function(prefix) {
+	_errorPrefix = prefix;
+    }
+
+    /**
+     * The sequence to be used as an error suffix
+     */
+    var _errorSuffix = "</b></font>";
+
+    /**
+     * The error suffix getter
+     *
+     * Return:
+     *   The String representation of error suffix
+     */
+    constructor.getErrorSuffix = function() {
+	return _errorSuffix.toString();
+    }
+
+    /**
+     * The error suffix setter
+     *
+     * Parameters:
+     *    suffix - the sequence to be used as error suffix
+     */
+    constructor.setErrorSuffix = function(suffix) {
+	_errorSuffix = suffix;
+    }
+
+    /**
+     * The sequence to be used as an warning prefix
+     */
+    var _warningPrefix = "<font color=\"black\"><b>Warning: ";
+    
+    /**
+     * The warning prefix sequence getter
+     * 
+     * Return:
+     *    The String represenataion of warning prefix
+     */
+    constructor.getWarningPrefix = function() {
+	return _warningPrefix.toString();
+    }
+
+    /**
+     * The warning prefix setter
+     *
+     * Parameters:
+     *    prefix - the sequence to be used as warning prefix
+     */
+    constructor.setWarningPrefix = function(prefix) {
+	_warningPrefix = prefix;
+    }
+
+    /**
+     * The sequence to be used as an warning suffix
+     */
+    var _warningSuffix = "</b></font>";
+
+    /**
+     * The warning suffix getter
+     *
+     * Return:
+     *   The String representation of warning suffix
+     */
+    constructor.getWarningSuffix = function() {
+	return _warningSuffix.toString();
+    }
+
+    /**
+     * The warning suffix setter
+     *
+     * Parameters:
+     *    suffix - the sequence to be used as warning suffix
+     */
+    constructor.setWarningSuffix = function(suffix) {
+	_warningSuffix = suffix;
+    }
+
+    /**
+     * The sequence to be used as an notify prefix
+     */
+    var _notifyPrefix = "<font color=\"green\">Notify: ";
+    
+    /**
+     * The notify prefix sequence getter
+     * 
+     * Return:
+     *    The String represenataion of notify prefix
+     */
+    constructor.getNotifyPrefix = function() {
+	return _notifyPrefix.toString();
+    }
+
+    /**
+     * The notify prefix setter
+     *
+     * Parameters:
+     *    prefix - the sequence to be used as notify prefix
+     */
+    constructor.setNotifyPrefix = function(prefix) {
+	_notifyPrefix = prefix;
+    }
+
+    /**
+     * The sequence to be used as an notify suffix
+     */
+    var _notifySuffix = "</font>";
+
+    /**
+     * The notify suffix getter
+     *
+     * Return:
+     *   The String representation of notify suffix
+     */
+    constructor.getNotifySuffix = function() {
+	return _notifySuffix.toString();
+    }
+
+    /**
+     * The notify suffix setter
+     *
+     * Parameters:
+     *    suffix - the sequence to be used as notify suffix
+     */
+    constructor.setNotifySuffix = function(suffix) {
+	_notifySuffix = suffix;
+    }
+
+    /**
+     * The sequence to be used as an trace prefix
+     */
+    var _tracePrefix = "<font color=\"blue\">Trace: ";
+    
+    /**
+     * The trace prefix sequence getter
+     * 
+     * Return:
+     *    The String represenataion of trace prefix
+     */
+    constructor.getTracePrefix = function() {
+	return _tracePrefix.toString();
+    }
+
+    /**
+     * The trace prefix setter
+     *
+     * Parameters:
+     *    prefix - the sequence to be used as trace prefix
+     */
+    constructor.setTracePrefix = function(prefix) {
+	_tracePrefix = prefix;
+    }
+
+    /**
+     * The sequence to be used as an trace suffix
+     */
+    var _traceSuffix = "</font>";
+
+    /**
+     * The trace suffix getter
+     *
+     * Return:
+     *   The String representation of trace suffix
+     */
+    constructor.getTraceSuffix = function() {
+	return _traceSuffix.toString();
+    }
+
+    /**
+     * The trace suffix setter
+     *
+     * Parameters:
+     *    suffix - the sequence to be used as trace suffix
+     */
+    constructor.setTraceSuffix = function(suffix) {
+	_traceSuffix = suffix;
+    }
+    
+    // Return an instance
+    return constructor;
+})();
+
+/**
+ * Special logging level that prevents any logs to be shown
+ */
 Logger.LEVEL_NONE = 0;
+/**
+ * Logging level that causes the error messages to appear
+ */
 Logger.LEVEL_ERROR = 1 << 0;
+/**
+ * Logging level that causes the warning messages to appear
+ */
 Logger.LEVEL_WARNING = 1 << 1;
+/**
+ * Logging level that causes the notify messages to appear
+ */
 Logger.LEVEL_NOTIFY = 1 << 2;
+/**
+ * Logging level that causes the trace messages to appear
+ */
 Logger.LEVEL_TRACE = 1 << 3;
+/**
+ * Special logging level that causes all messages to appear
+ */
 Logger.LEVEL_ALL = Logger.LEVEL_ERROR
     | Logger.LEVEL_WARNING
     | Logger.LEVEL_NOTIFY
     | Logger.LEVEL_TRACE;
 
-// The logging level to use
+/**
+ * The current logging level of the Logger class. This variable shall be set to
+ * one of te above levels in order to make some log entries to appear. The logging
+ * level is interpreted as a bit mask so it is possible to log any combination of
+ * logging levels.
+ */
 Logger.loggingLevel = Logger.LEVEL_NONE;
 
-// The logging destination output element. It shall be redefined by the user.
+/**
+ * The logging destination output element. It shall be redefined by the user
+ * in order to display logging messages
+ */
 Logger.output = null;
 
-// The logger name
-Logger.prototype._name;
-Logger.prototype.getName = function() {
-    return "[" + this._name + "]";
-}
-Logger.prototype._setName = function(name) {
-    this._name = name;
-}
-
-// One message number for all Loggers. It will be added for all messages
-Logger._messageNumber = 0;
-
-// If the current logging level is sufficient, appends the message
-// to the current output DIV innerHTML
-Logger.prototype._print = function(message, level) {
-    // Print only if _loggingLevel is higher than LEVEL_NONE
-    // and level is is lower or equal to _loggingLevel
-    if (Logger.output != null
-        && Logger.loggingLevel != Logger.LEVEL_NONE
-	&& (Logger.loggingLevel & level) == level) {
-	Logger._messageNumber++;
-	var outputHTML = Logger.output.innerHTML;
-	outputHTML += Logger._messageNumber + ": " + message;
-	Logger.output.innerHTML = outputHTML;
-    }
-}
-
-// Parses the given array and returns its string represenatation
-// The output is in the following form:
-// {key1:value1, key2:value2}
-Logger.prototype._parseArray = function(array) {
-    var retString = "";
-    if (null != array) {
-	retString = "{";
-	for (key in array) {
-	    retString += key.toString() + ":" + array[key].toString() + ", ";
-	}
-	retString = retString.substring(0, retString.lastIndexOf(","));
-	retString += "}";
-    }
-    return retString;
-}
-
-// Endline character
-Logger.prototype._endl = "<br />";
-Logger.prototype.getEndl = function() {
-    return this._endl;
-}
-Logger.prototype.setEndl = function(endl) {
-    this._endl = endl;
-}
-
-// Error
-Logger.prototype._errorPrefix = "<font color=\"red\"><b>Error: ";
-Logger.prototype.getErrorPrefix = function() {
-    return this._errorPrefix;
-}
-Logger.prototype.setErrorPrefix = function(prefix) {
-    this._errorPrefix = prefix;
-}
-Logger.prototype._errorSuffix = "</b></font>";
-Logger.prototype.getErrorSuffix = function() {
-    return this._errorSuffix;
-}
-Logger.prototype.setErrorSuffix = function(suffix) {
-    this._errorSuffix = suffix;
-}
-Logger.prototype.error = function(message, parameters) {
-    if ((Logger.loggingLevel & Logger.LEVEL_ERROR) == Logger.LEVEL_ERROR) {
-	var params = ": " + this._parseArray(parameters);
-	var msg = this.getErrorPrefix() + message + params + this.getErrorSuffix() + this.getName() + " " + this.getEndl();
-	this._print(msg, Logger.LEVEL_ERROR);
-    }
-}
-
-// Warning
-Logger.prototype._warningPrefix = "<b>Warning: ";
-Logger.prototype.getWarningPrefix = function() {
-    return this._warningPrefix;
-}
-Logger.prototype.setWarningPrefix = function(prefix) {
-    this._warningPrefix = prefix;
-}
-Logger.prototype._warningSuffix = "</b>";
-Logger.prototype.getWarningSuffix = function() {
-    return this._warningSuffix;
-}
-Logger.prototype.setWarningSuffix = function(suffix) {
-    this._warningSuffix = suffix;
-}
-Logger.prototype.warning = function(message, parameters) {
-    if ((Logger.loggingLevel & Logger.LEVEL_WARNING) == Logger.LEVEL_WARNING) {
-	var params = ": " + this._parseArray(parameters);
-	var msg = this.getWarningPrefix() + message + params + this.getWarningSuffix() + this.getName() + " " + this.getEndl();
-	this._print(msg, Logger.LEVEL_WARNING);
-    }
-}
-
-// Notify
-Logger.prototype._notifyPrefix = "Note: ";
-Logger.prototype.getNotifyPrefix = function() {
-    return this._notifyPrefix;
-}
-Logger.prototype.setNotifyPrefix = function(prefix) {
-    this._notifyPrefix = prefix;
-}
-Logger.prototype._notifySuffix = "";
-Logger.prototype.getNotifySuffix = function() {
-    return this._notifySuffix;
-}
-Logger.prototype.setNotifySuffix = function(suffix) {
-    this._notifySuffix = suffix;
-}
-Logger.prototype.notify = function(message, parameters) {
-    if ((Logger.loggingLevel & Logger.LEVEL_NOTIFY) == Logger.LEVEL_NOTIFY) {
-	var params = ": " + this._parseArray(parameters);
-	var msg = this.getNotifyPrefix() + message + params + this.getNotifySuffix() + this.getName() + " " + this.getEndl();
-	this._print(msg, Logger.LEVEL_NOTIFY);
-    }
-}
-
-// Trace
-Logger.prototype._tracePrefix = "<font color=\"blue\">";
-Logger.prototype.getTracePrefix = function() {
-    return this._tracePrefix;
-}
-Logger.prototype.setTracePrefix = function(prefix) {
-    this._tracePrefix = prefix;
-}
-Logger.prototype._traceSuffix = "</font>";
-Logger.prototype.getTraceSuffix = function() {
-    return this._traceSuffix;
-}
-Logger.prototype.setTraceSuffix = function(suffix) {
-    this._traceSuffix = suffix;
-}
-Logger.prototype.trace = function(message, parameters) {
-        if ((Logger.loggingLevel & Logger.LEVEL_TRACE) == Logger.LEVEL_TRACE) {
-	    var params = ": " + this._parseArray(parameters);
-	    var msg = this.getTracePrefix() + message + params + this.getTraceSuffix() + this.getName() + " " + this.getEndl();
-	    this._print(msg, Logger.LEVEL_TRACE);
-	}
-}
 
 // Logger tests
 function _testLogger(div) {
